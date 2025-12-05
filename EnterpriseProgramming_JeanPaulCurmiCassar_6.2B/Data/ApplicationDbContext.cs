fee32f74d996115925bdcd83bc17082172fb5116
Jean Paul Curmi Cassar <jean.curmi.h64031@mcast.edu.mt>
2025-12-05 19:26:55 +0100

    Add project files.

---
+++ EnterpriseProgramming_JeanPaulCurmiCassar_6.2B/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace EnterpriseProgramming_JeanPaulCurmiCassar_6._2B.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ EnterpriseProgramming_JeanPaulCurmiCassar_6.2B/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EnterpriseProgramming_JeanPaulCurmiCassar_6._2B.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
